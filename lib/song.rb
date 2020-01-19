f4366365026250cbae58c971ed620ab0544cbd3c
Asija Watson <asija.w@yahoo.com>
2020-01-19 02:15:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,13 +14,13 @@ class Song
   def self.new_by_filename(file)
     song = file.split(" - ")[1]
     artist = file.split(" - ")[0]
-    new_song = self.new(song_name)
+    new_song = self.new(song)
     new_song.artist_name = artist
     new_song
   end
   
   def artist_name=(name)
-    @artist = Artist.find_or_create_by_name(name)
-    @artist.add_song(self)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
 end 
