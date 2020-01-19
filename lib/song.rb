eb068cd825595148714b0f7de1ca2d483425e9a3
Asija Watson <asija.w@yahoo.com>
2020-01-19 02:09:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,10 @@
 class Song 
   attr_accessor :name, :artist 
-  #@@all = []
+  @@all = []
   
   def initialize(name)
     @name = name 
-   # @@all << self 
+    @@all << self 
   end 
   
   def self.all 
